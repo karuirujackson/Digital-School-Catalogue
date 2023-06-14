0662b807414fab7b11730ddf8d60522e87382017
Karuiru Jackson <jkjackaruiru641@gmail.com>
2023-06-14 17:17:32 +0300

    Create Classes School-Primary- Middle-High

---
+++ app.js
@@ -0,0 +1,23 @@
+class School {
+    constructor (){
+
+    }
+};
+
+class Primary extends School{
+    constructor (){
+
+    }
+};
+
+class Middle extends School{
+    constructor (){
+
+    }
+};
+
+class High extends School {
+    constructor (){
+
+    }
+};
