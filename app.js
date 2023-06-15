6a416cab7668e711d5a6a698a48650493ca8a775
Karuiru Jackson <jkjackaruiru641@gmail.com>
2023-06-15 10:39:05 +0300

    Instances of PrimarySchool, HighSchol and School

--- app.js
+++ app.js
@@ -14,25 +14,24 @@ class School {
         return this._numberOfStudents;
     }
     set numberOfStudents(num){
-        if(typeof num === 'number'){
-            return this._numberOfStudents = num;
-        }else{
+        if(num.isNaN()){
             return 'Invalid input: numberOfStudents must be set to a Number.'
+        }else{
+            return this._numberOfStudents = num;
         }
     }
     quickFacts (){
-        return `${this._name} educates ${this._numberOfStudents} students at the ${this._level} school ${this._level}`
+        return `${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level`
     }
     static pickSubstituteTeacher (substituteTeachers){
-        substituteTeachers = [];
         let randomTeacher = Math.floor(Math.random() * substituteTeachers.length);
-        return randomTeacher;
+        return substituteTeachers[randomTeacher];
     }
 };
 
-class Primary extends School{
+class PrimarySchool extends School{
     constructor (name, numberOfStudents, pickupPolicy){
-        super(name);
+        super(name, 'primary', numberOfStudents);
         this._pickupPolicy = pickupPolicy;
     }
     get pickupPolicy(){
@@ -40,19 +39,40 @@ class Primary extends School{
     }
 };
 
-class Middle extends School{
-    constructor (name){
-        super(name);
+class MiddleSchool extends School{
+    constructor (name, numberOfStudents){
+        super(name, 'Middle', numberOfStudents);
     }
 };
 
-class High extends School {
-    constructor (){
-        this._sportsTeams = []
+class HighSchool extends School {
+    constructor (name, numberOfStudents, sportsTeams){
+        super(name, 'High', numberOfStudents);
+        this._sportsTeams = sportsTeams;
     }
+    get sportsTeams(){
+        this._sportsTeams;
+    } 
 };
+
+
+
+//const testbugnumberOfStudents = new School('Jay', 'ABC');
+//console.log(testbugnumberOfStudents);
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514,'Students must be picked up by a parent, guardian, or a family member over the age of 13.')
+console.log(lorraineHansbury);
+//console.log(lorraineHansbury.quickFacts());
+
+const selectTeacher = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+console.log(selectTeacher);
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+console.log(alSmith.sportsTeams);
+
+
+
 
 
 /*myArray = ['A', 'B', 'C', 'D'];
 let randomArray = Math.floor(Math.random() * myArray.length);
-console.log(randomArray);*/
+console.log(myArray[randomArray]);*/
